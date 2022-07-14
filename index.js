275240aa43e647351cae8a5b4e883eb6a8767055
Isai <castanedaisai303@gmail.com>
2022-07-14 10:19:49 -0700

    commit tree 5399b3518a0dbd737c8811317e9b72c19cf0a489

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+//const { DEFAULT_EXTENSIONS } = require("@babel/core");
+
+// Write your solution in this film
+const employee = {
+        name: "Sam",
+        streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    const newEmployee = { ...employee};
+
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newerEmployee = { ...employee};
+    delete newerEmployee[key];
+    return newerEmployee
+}
+`deleteFromEmployeeByKey() {
+let employee   
+    delete employee.key;
+    console.log(employee.key)
+}`
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+ delete employee[key];
+return employee
+}
